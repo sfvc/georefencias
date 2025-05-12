7a308617ffd9f79c779ee4467deb3cfd861597df
Agustin Jalil <agustinjalilleon@gmail.com>
2025-05-12 10:45:21 -0300

    netlify instalado con npx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [react()],
-  
+
   devToolbar: {
     enabled: false,
   },
+
+  adapter: netlify(),
 });
